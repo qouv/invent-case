1cb9cda4f74456cd5bb8d4b7b98e9f89f7c92a6e
Omer Aslan <32545727+qouv@users.noreply.github.com>
2025-02-14 20:17:01 +0300

    fix: remove response body

--- src/routes/books/booksController.ts
+++ src/routes/books/booksController.ts
@@ -6,9 +6,9 @@ import { eq } from 'drizzle-orm'
 
 export async function createBook (req: Request, res: Response) {
 	try {
-		const [createdBook] = await db.insert(booksTable).values(req.cleanBody).returning()
+		await db.insert(booksTable).values(req.cleanBody).returning()
 
-		res.status(201).json(createdBook)
+		res.sendStatus(201)
 	} catch (e) {
 		res.status(500).send(e)
 	}
@@ -26,22 +26,22 @@ export async function listBooks (req: Request, res: Response) {
 
 export async function getBookInfoById (req: Request, res: Response) {
 	try {
-		const { id } = req.params
+		const { id: bookId } = req.params
 
 		// Check if id is a number
-		if (isNaN(Number(id))) {
+		if (isNaN(Number(bookId))) {
 			res.status(400).json({ error: 'Invalid id' });
 			return
 		}
 
-		const [book] = await db.select().from(booksTable).where(eq(booksTable.id, Number(id)))
+		const [book] = await db.select().from(booksTable).where(eq(booksTable.id, Number(bookId)))
 
 		if (!book) {
 			res.status(404).json({ error: 'Book not found' })
 			return
 		}
 
-		const borrowedBooks = await db.select().from(borrowedBooksTable).where(eq(borrowedBooksTable.book_id, Number(id)))
+		const borrowedBooks = await db.select().from(borrowedBooksTable).where(eq(borrowedBooksTable.book_id, Number(bookId)))
 		const score = borrowedBooks.length ? borrowedBooks.reduce((acc, curr) => acc + (curr.score ?? 0), 0) / borrowedBooks.length : -1
 
 		res.json({ id: book.id, name: book.name, score })
