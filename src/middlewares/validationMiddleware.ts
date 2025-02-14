1461de5977c78e5e9f14057cd3530b36c9cb3d62
Omer Aslan <32545727+qouv@users.noreply.github.com>
2025-02-14 15:29:17 +0300

    refactor: check id type

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -2,23 +2,29 @@ import { Request, Response, NextFunction } from 'express';
 import _ from 'lodash';
 import { z, ZodError } from 'zod';
 
-export function validateData(schema: z.ZodObject<any, any>) {
+export function validateData(schema: z.ZodObject<any, any> = z.object({})) {
 	return (req: Request, res: Response, next: NextFunction) => {
-	try {
-		schema.parse(req.body);
-		req.cleanBody = _.pick(req.body, Object.keys(schema.shape));
+		try {
+			// Check if id is a number
+			if (req.params.id && isNaN(Number(req.params.id))) {
+				res.status(400).json({ error: 'Invalid id' });
+			}
 
-		next();
-	} catch (error) {
-		if (error instanceof ZodError) {
-			const errorMessages = error.errors.map((issue: any) => ({
-				message: `${issue.path.join('.')} is ${issue.message}`,
-			}));
+			// Clean the request body from any extra fields
+			schema.parse(req.body);
+			req.cleanBody = _.pick(req.body, Object.keys(schema.shape));
 
-			res.status(400).json({ error: 'Invalid data', details: errorMessages });
-		} else {
-			res.status(500).json({ error: 'Internal Server Error' });
+			next();
+		} catch (error) {
+			if (error instanceof ZodError) {
+				const errorMessages = error.errors.map((issue: any) => ({
+					message: `${issue.path.join('.')} is ${issue.message}`,
+				}));
+
+				res.status(400).json({ error: 'Invalid data', details: errorMessages });
+			} else {
+				res.status(500).json({ error: 'Internal Server Error' });
+			}
 		}
-	}
   };
 }
